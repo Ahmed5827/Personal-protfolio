300ac253110494507260aca1779776f2e2ef4f5a
Chebbi Ahmed <ssjzchebbi@gmail.com>
2025-04-01 19:58:22 +0100

    unabling speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
 import { Analytics } from '@vercel/analytics/next';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -51,6 +51,7 @@ export default function RootLayout({
         <Navbar />
         <main className=" min-h-screen">{children}</main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
